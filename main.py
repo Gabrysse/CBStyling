fffe75a60ebfe75d2e96865046ae56c931517acd
Gabriele <gabrielerosi97@gmail.com>
2021-06-25 21:26:23 +0000

    Removed paper files and added some comment to main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import utils as ut
+from torchvision import transforms
+from PIL import Image
 
 
 def main(img_fname, out_fname, style_id, class_id):
@@ -10,21 +12,40 @@ def main(img_fname, out_fname, style_id, class_id):
     # ======================================
     # Seg part
     image = ut.load_image(img_fname)
+    print(image.shape)
+
     semseg = ut.get_semseg_image(seg_model, image)
+    print(semseg.shape)
+
     # Get image with mask showing class_id 13, which is cars by default
     print("Stylizing class %d..." % class_id)
     fg_image = ut.get_masked_image(seg_model, image, category=class_id, bg=0)
+    ut.save_image("images/fg_image.png", fg_image)
+
     # Get image with mask showing everything except class_id 13
     bg_image = ut.get_masked_image(seg_model, image, category=class_id, bg=1)
+    ut.save_image("images/bg_image.png", bg_image)
 
     # ======================================
     # Style part
+    # load_image_style carica l'immagine in input, la mette in un tensore e la
+    # carica nella GPU
+    # size = torch.Size([1, 3, 1024, 2048])
     image = ut.load_image_style(img_fname, scale=1.0)
+    print(image.size())
+
+    # Qui invece viene applicato lo stile all'immagine caricata prima
     image_style1 = ut.get_styled_image(style_model, image)
+
     # Apply local style to fg
+    # Applica lo stile solo alla classe scelta 
     fg_styled = image_style1 * (fg_image != 0)
+    ut.save_image("images/fg_styled.png", fg_styled)
+
     # Apply local style to bg
+    # Applica lo stile a tutto il resto dell'immagine
     bg_styled = image_style1 * (bg_image != 0)
+    ut.save_image("images/bg_styled.png", bg_styled)
 
     # ======================================
     # Save part
